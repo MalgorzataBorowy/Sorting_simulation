d181c041fb3b148453b13bab4eff75c6501a63b3
MalgorzataBorowy <borowy.malgorzata@gmail.com>
2020-09-28 21:30:36 +0200

    Initial commit

---
+++ Sorting/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea11 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea11.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea11);
+            this.chart.Location = new System.Drawing.Point(12, 12);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(694, 488);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(712, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bubble Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnBubbleSort_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(712, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Quick Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnQuickSort_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(712, 450);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(712, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Bucket Sort";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnBucketSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 512);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Sorting/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Threading;
+
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+        static int elements = 50;
+        int[] array = new int[elements];
+        string[] series = new String[elements];
+        Series chartSeries;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            Random rnd = new Random();
+            for (int i=0; i<array.Length; i++)
+            {
+                series[i] = i.ToString();
+                array[i] = rnd.Next(1, 100);
+            }
+            Visualize();
+        }
+
+        public void Quick()
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        public void Bubble()
+        {
+            BubbleSort(array);
+        }
+
+        public void Bucket()
+        {
+            BucketSort(ref array);
+        }
+
+        private void QuickSort(int[] tab, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int x = tab[(left + right) >> 1];
+            do
+            {
+                while (tab[i] < x) i++;
+                while (tab[j] > x) j--;
+                if (i <= j)
+                {
+                    int temp = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = temp;
+                    i++;
+                    j--;
+                }
+                if (chart.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { Visualize(); });
+                }
+            } while (i <= j);
+            if (left < j) QuickSort(tab, left, j);
+            if (right > i) QuickSort(tab, i, right);
+        }
+
+        public void BubbleSort(int[] ar)
+        {
+            for (int pass = 1; pass < ar.Length; pass++)
+            {
+                for (int i = 0; i < ar.Length - 1; i++)
+                {
+                    if (ar[i] > ar[i + 1])
+                    {
+                        int hold = ar[i];
+                        ar[i] = ar[i + 1];
+                        ar[i + 1] = hold;
+                    }
+                    if (chart.IsHandleCreated)
+                    {
+                        this.Invoke((MethodInvoker)delegate { Visualize(); });
+                    }
+                }
+            }
+        }
+
+        public void BucketSort(ref int[] x)
+        {
+            List<int> result = new List<int>();
+            int numOfBuckets = 10;
+            
+            List<int>[] buckets = new List<int>[numOfBuckets];
+            for (int i = 0; i < numOfBuckets; i++)
+                buckets[i] = new List<int>();
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                int buckitChoice = (x[i] / numOfBuckets);
+                buckets[buckitChoice].Add(x[i]);
+            }
+
+            for (int i = 0; i < numOfBuckets; i++)
+            {
+                int[] temp = BubbleSortList(buckets[i]);
+                result.AddRange(temp);
+            }
+            x = result.ToArray();
+        }
+
+        public int[] BubbleSortList(List<int> input)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input.Count; j++)
+                {
+                    if (input[i] < input[j])
+                    {
+                        int temp = input[i];
+                        input[i] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+            return input.ToArray();
+        }
+
+        public void Visualize()
+        {
+            chart.Series.Clear();
+            for (int i = 0; i < array.Length; i++)
+            {
+                chartSeries = chart.Series.Add(series[i]);
+                chartSeries.Points.Add(array[i]);
+                chart.Series[i].Color = Color.Blue;
+            }
+        }
+
+        private void btnBubbleSort_Click(object sender, EventArgs e)
+        {
+            Thread myThread = new Thread(new ThreadStart(this.Bubble));
+            myThread.IsBackground = true;
+            myThread.Start();
+            Visualize();
+        }
+
+        private void btnQuickSort_Click(object sender, EventArgs e)
+        {
+            Thread myThread = new Thread(new ThreadStart(this.Quick));
+            myThread.IsBackground = true;
+            myThread.Start();
+            Visualize();
+        }
+
+        private void btnBucketSort_Click(object sender, EventArgs e)
+        {
+            Thread myThread = new Thread(new ThreadStart(this.Bucket));
+            myThread.IsBackground = true;
+            myThread.Start();
+            Visualize();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {        
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+            }
+            Visualize();
+        }
+    }
+}
