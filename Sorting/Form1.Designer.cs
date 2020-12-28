c9e59f371dbfb87ea941dbbbaa3aa6bc40ddc83f
MalgorzataBorowy <borowy.malgorzata@gmail.com>
2020-12-28 16:29:08 +0100

    readme

--- Sorting/Form1.Designer.cs
+++ Sorting/Form1.Designer.cs
@@ -105,7 +105,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sorting algorithms";
             ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
 
---
+++ Sorting/rm/README.md
@@ -0,0 +1,12 @@
+# Sudoku
+
+This is an application that visualizes some of the sorting algorithms.
+
+## Technologies
+The app was written using C# and Windows Forms (Microsoft .NET Framework).
+
+## Setup
+For download this app click here.
+
+## Screenshots
+![window](/window.jpg)
